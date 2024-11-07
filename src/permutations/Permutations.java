dfff3b56bd37304cd85f823642f5c67d14a09765
NingxiY829 <yang.ningx@northeastern.edu>
2024-11-06 19:00:17 -0800

    generatePermutation update

--- src/permutations/Permutations.java
+++ src/permutations/Permutations.java
@@ -9,25 +9,47 @@ public class Permutations implements BackAndForthIterator{
   private final List<String> permutations;
 
   public Permutations(String sequence) {
-    if (sequence == null || sequence.length() == 0) {
+    if (sequence == null || sequence.isEmpty()) {
       throw new IllegalArgumentException();
     }
     permutations = new ArrayList<>();
-    generatePermutations();
+    generatePermutations("", sequence);
     this.currentIndex = -1;
   }
 
   public Permutations(String sequence, int startLength) {
-    if (sequence == null || sequence.length() == 0) {
+    if (sequence == null || sequence.isEmpty()
+            || startLength < 1 || startLength > sequence.length()) {
       throw new IllegalArgumentException();
     }
     permutations = new ArrayList<>();
-    generatePermutations();
+
+    generatePermutations("", sequence, startLength);
     this.currentIndex = -1;
   }
 
-  public void generatePermutations() {
+  public void generatePermutations(String prefix, String suffix) {
+    int n = suffix.length();
+    if (n == 0) {
+      permutations.add(prefix);
+    } else {
+      for (int i = 0; i < n; i++) {
+        generatePermutations(prefix + suffix.charAt(i),
+                suffix.substring(0, i) + suffix.substring(i+1, n));
+      }
+    }
+  }
 
+  public void generatePermutations(String prefix, String suffix, int permutationLength) {
+    int n = suffix.length();
+    if (n == permutationLength) {
+      permutations.add(prefix);
+    } else {
+      for (int i = 0; i < n; i++) {
+        generatePermutations(prefix + suffix.charAt(i),
+                suffix.substring(0, i) + suffix.substring(i+1, n));
+      }
+    }
   }
 
 
