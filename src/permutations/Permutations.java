56a5ebab96bd23a7f4f7b5e5b85c338056bb4961
NingxiY829 <yang.ningx@northeastern.edu>
2024-11-06 23:48:02 -0800

    calculate Permutation

--- src/permutations/Permutations.java
+++ src/permutations/Permutations.java
@@ -1,20 +1,21 @@
 package permutations;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Permutations implements BackAndForthIterator{
   private int currentIndex;
-  private final List<String> permutations;
+  private final int startLength;
+  private final String sequence;
+  private int permutationNumber;
 
   public Permutations(String sequence) {
     if (sequence == null || sequence.isEmpty()) {
       throw new IllegalArgumentException();
     }
-    permutations = new ArrayList<>();
-    generatePermutations("", sequence);
+    this.sequence = sequence;
+    this.startLength = 1;
     this.currentIndex = -1;
+    calculatePermutationNumber();
   }
 
   public Permutations(String sequence, int startLength) {
@@ -22,50 +23,46 @@ public class Permutations implements BackAndForthIterator{
             || startLength < 1 || startLength > sequence.length()) {
       throw new IllegalArgumentException();
     }
-    permutations = new ArrayList<>();
-
-    generatePermutations("", sequence, startLength);
+    this.sequence = sequence;
+    this.startLength = startLength;
     this.currentIndex = -1;
+    calculatePermutationNumber();
   }
 
-  public void generatePermutations(String prefix, String suffix) {
-    int n = suffix.length();
-    if (n == 0) {
-      permutations.add(prefix);
-    } else {
-      for (int i = 0; i < n; i++) {
-        generatePermutations(prefix + suffix.charAt(i),
-                suffix.substring(0, i) + suffix.substring(i+1, n));
-      }
+  public void calculatePermutationNumber() {
+    int total = 0;
+    for (int k = startLength; k <= sequence.length(); k++) {
+      total += factorial(sequence.length()) / factorial(sequence.length() - k);
     }
+    permutationNumber = total;
   }
 
-  public void generatePermutations(String prefix, String suffix, int permutationLength) {
-    int n = suffix.length();
-    if (n == permutationLength) {
-      permutations.add(prefix);
-    } else {
-      for (int i = 0; i < n; i++) {
-        generatePermutations(prefix + suffix.charAt(i),
-                suffix.substring(0, i) + suffix.substring(i+1, n));
-      }
+  private int factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+      result *= i;
     }
+    return result;
   }
 
 
   @Override
   public Object previous() throws NoSuchElementException {
+    if (!hasPrevious()) {
+      throw new NoSuchElementException();
+    }
+    currentIndex--;
     return null;
   }
 
   @Override
   public boolean hasPrevious() {
-    return false;
+    return currentIndex > 0;
   }
 
   @Override
   public boolean hasNext() {
-    return false;
+    return currentIndex < permutationNumber;
   }
 
   @Override
